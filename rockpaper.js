852658db4aaa299760bebd41faacad7164849b46
Hoang To <hoangto.losangeles@gmail.com>
2023-03-17 22:50:46 -0700

    finish rock paper scissor

--- rockpaper.js
+++ rockpaper.js
@@ -1,7 +1,7 @@
 function getComputerChoice (){
-    choice_list = ["ROCK", "PAPER", "SCISSOR"];
+    choice_list = ["ROCK", "PAPER", "SCISSORS"]; // create a list of possible choice
     var computerChoice;
-    r_pick = random();
+    r_pick = math.random();
     if (r_pick <= 1/3){
         computerChoice = choice_list[0];
     } else if (r_pick <= 2/3) {
@@ -12,42 +12,48 @@ function getComputerChoice (){
     console.log(computerChoice);
     return computerChoice;
 }
-function playRound(playerSelection, computerSelection, score_player, score_comp) {
+
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
-    choice_list = ["ROCK", "PAPER", "SCISSOR"];
+    choice_list = ["ROCK", "PAPER", "SCISSORS"];
     let index_comp = choice_list.indexOf(computerSelection);
     let index_player = choice_list.indexOf(playerSelection);
     if (index_comp == index_player){
-        return console.log("You Draw!");
+        return console.log("You Draw! Both of you chose ", choice_list[index_comp]);
     } 
     if ((index_comp - index_player) == 1 || (index_comp - index_player) == -2){
         score_comp++;
-        return console.log("You Lose! ", index_comp, " beats ", index_player);
+        return console.log("You Lose! ", choice_list[index_comp], " beats ", choice_list[index_player]);
     }
     if ((index_comp - index_player) == -1 || (index_comp - index_player) == 2){
         score_player++;
-        return console.log("You Won! ", index_player, " beats ", index_comp);
+        return console.log("You Won! ", choice_list[index_player], " beats ", choice_list[index_comp]);
     }
-    return console.log("Please enter valid choice between Rock, Paper, or Scissor");
 }
 
 function game(){
-    let score_player = 0;
-    let score_comp = 0;
+    choice_list = ["ROCK", "PAPER", "SCISSORS"];
     for (let i = 0; i < 5; i++){
-        let playerSelection = readLine(prompt("Please enter valid choice between Rock, Paper, or Scissor:"));
+        let playerSelection = prompt("Please enter valid choice between Rock, Paper, or Scissor:");
+            while (!(choice_list.indexOf(playerSelection.toUpperCase()) >= 0)){
+                playerSelection = prompt("Please enter valid choice between Rock, Paper, or Scissor:")
+            } 
         let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection, score_player, score_comp);
+        playRound(playerSelection, computerSelection);
+        console.log("Current Score: Player ", score_player, ", Computer: ", score_comp)
     }
     if (score_player > score_comp){
-        return console.log("You Win!")
+        return console.log("You Win!"," Final Score: Player ", score_player, ", Computer: ", score_comp)
     }
 
     if (score_player < score_comp){
-        return console.log("You Lose!")
+        return console.log("You Lose!", " Final Score: Player ", score_player, ", Computer: ", score_comp)
     }
 
-    if (score_player > score_comp){
-        return console.log("You Tied with Comptuter!")
+    if (score_player == score_comp){
+        return console.log("You Tied with Comptuter!"," Final Score: Player ", score_player, ", Computer: ", score_comp)
     }
 }
+var score_player = 0;
+var score_comp = 0;
+game();
